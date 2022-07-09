a18b6d1fe59a65e79857fb59165715470ccea3b8
kage2k <flutterkage2k@gmail.com>
2022-07-09 12:12:44 +0900

    somthing testing

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebase06092121/auth/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -8,6 +10,16 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await signOut();
+            },
+            icon: Icon(
+              Icons.logout,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase06092121/auth/auth.dart';
 import 'package:firebase06092121/home_page.dart';
+import 'package:firebase06092121/screens/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -28,7 +30,16 @@ class MyApp extends StatelessWidget {
             Theme.of(context).textTheme,
           ),
         ),
-        home: const Login()
+        home: StreamBuilder(
+          stream: authInstance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Home();
+            }
+            return Login();
+          },
+        )
+
         // initialRoute: '/welcome_screen',
         // routes: {
         //   '/welcome_scrren':(context) => const Welcome_screen(),
@@ -36,123 +47,5 @@ class MyApp extends StatelessWidget {
 
         // },
         );
-  }
-}
-
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
-
-  @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  late String email;
-  late String password;
-  final emailController = TextEditingController();
-  final passController = TextEditingController();
-  final formKey = GlobalKey<FormState>();
-
-  //firebase
-  final auth = FirebaseAuth.instance;
-
-  late GoogleSignInAccount userObj;
-
-  @override
-  void dispose() {
-    emailController.dispose();
-    passController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Future<void> login() async {
-      try {
-        await auth.signInWithEmailAndPassword(email: email, password: password);
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text("Signed In"),
-            duration: Duration(milliseconds: 1000),
-          ),
-        );
-      } on FirebaseAuthException catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text("Failed with code: ${e.code}"),
-          duration: const Duration(milliseconds: 1000),
-        ));
-      }
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Form(
-        key: formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(borderSide: BorderSide(color: Colors.teal)),
-                  hintText: 'email',
-                ),
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an Email';
-                  }
-                  if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
-                    return 'Please enter a valid Email';
-                  }
-                },
-              ),
-              TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(borderSide: BorderSide(color: Colors.teal)),
-                    hintText: 'password',
-                  ),
-                  controller: passController,
-                  keyboardType: TextInputType.visiblePassword,
-                  validator: (value) => value!.length < 6 ? 'Please is too short' : null),
-              TextButton(
-                child: Text('Login'),
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    email = emailController.text;
-                    password = passController.text;
-                    login();
-                  }
-                },
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextButton(
-                onPressed: () {
-                  _signInWithGoogle();
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-                },
-                child: Text('Google Sign In'),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  _signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
-
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
